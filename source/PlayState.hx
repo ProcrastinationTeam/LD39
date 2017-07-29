62ba3855faeaa2928191b270fb89bdd8d5493a44
LeRyokan <tixier.lucas@gmail.com>
2017-07-29 04:09:51 +0200

    Init Test Map
    Loading de la premiere map !

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -5,17 +5,44 @@ import flixel.FlxSprite;
 import flixel.FlxState;
 import flash.display.BitmapData;
 import flash.display.Bitmap;
+import flixel.addons.editors.ogmo.FlxOgmoLoader;
 import flixel.math.FlxPoint;
+import flixel.tile.FlxTilemap;
 
-
+private var _map : FlxOgmoLoader;
+private var _mWalls : FlxTilemap;
+private var _player : Player;
 
 class PlayState extends FlxState
 {
 
-	
 	override public function create():Void
 	{
 		super.create();
+
+		_map = new FlxOgmoLoader(AssetPaths.level1__oel);
+		_mWalls = _map.loadTilemap(AssetPaths.tiles__png, 16, 16, "background");
+		_mWalls.follow();
+		_mWalls.setTileProperties(1, FlxObject.NONE);
+		_mWalls.setTileProperties(38, FlxObject.ANY);
+		_mWalls.setTileProperties(37, FlxObject.ANY);
+		add(_mWalls);
+
+		_player = new Player();
+		_map.loadEntities(placeEntities, "entities");
+		add(_player);
+
+	}
+
+	private function placeEntities(entityName:String, entityData:Xml):Void
+	{
+		var x:Int = Std.parseInt(entityData.get("x"));
+		var y:Int = Std.parseInt(entityData.get("y"));
+		if (entityName == "player")
+		{
+			_player.x = x;
+			_player.y = y;
+		}
 	}
 
 	override public function update(elapsed:Float):Void
