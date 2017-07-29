d0c62aec290d3867db89509c7b7d0e25a2f52741
Epono <guillaume.ambrois@gmail.com>
2017-07-29 18:38:54 +0200

    Ajout baissage batteire par hackers

--- source/Battery.hx
+++ source/Battery.hx
@@ -6,12 +6,16 @@ import flixel.FlxG;
 
 class Battery extends FlxSprite
 {
-	public var _batteryLevel:Float;
-	public var _batteryDecreaseRatePerSecond:Float;
+	public var _batteryLevel							: Float;
+	public var _batteryDecreaseRatePerSecond			: Float;
 
 	// Si on est entrain de parler avec maman, ça pompe la batterie
-	public var _isInCallWithMom:Bool = false;
-	public var _batteryDecreaseRatePerSecondThroughCall:Float = 0.5;
+	public var _isInCallWithMom							: Bool = false;
+	public var _batteryDecreaseRatePerSecondThroughCall	: Float = 0.5;
+	
+	// Pompage de batterie / hacker / seconde
+	private var _numberOfHackersHacking 					: Int = 0;
+	private var _batteryDecreaseRatePerSecondPerHacker		: Float = 0.2;
 
 	public function new(InitialBatteryValue:Float, BatteryDecreaseRatePerSecond:Float)
 	{
@@ -28,12 +32,17 @@ class Battery extends FlxSprite
 		super.update(elapsed);
 		//
 
+		// La batterie diminue toute seule par défaut
 		_batteryLevel -= _batteryDecreaseRatePerSecond * elapsed;
 		
+		// Si on est en call avec maman, la batterie descend
 		if (_isInCallWithMom)
 		{
 			_batteryLevel -= _batteryDecreaseRatePerSecondThroughCall * elapsed;
 		}
+		
+		// Si on se fait hack, la batterie se fait voler
+		_batteryLevel -= _numberOfHackersHacking * _batteryDecreaseRatePerSecondPerHacker * elapsed;
 	}
 
 	// Quand on se fait pomper de la batterie de façon ponctuelle, ça screenshake un coup
