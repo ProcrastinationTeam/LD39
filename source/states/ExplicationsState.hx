ecced9d4dcfdcc4e2ea829a7eec2daf5a3d28141
Epono <guillaume.ambrois@gmail.com>
2017-08-01 05:01:50 +0200

    Fix touches

--- source/states/ExplicationsState.hx
+++ source/states/ExplicationsState.hx
@@ -28,7 +28,7 @@ class ExplicationsState extends FlxState
 		_startDisplay.y = 440;
 		add(_startDisplay);
 
-		_hurryText = new FlxTypeText(20, 20, 600, "Hurry!\n\nYour phone battery is almost dead, only 98% remaning!\n\nDon't let it empty because Big Mommy is Watching (Over) You and will punish you if that happens.\n\nAnswer her messages ([M]) before she calls you and drains even more battery.\n\nAvoid thugs who will bully you around.\n\nHackers disguised as NPCs will follow you and steal your wifi, bully them ([X]) to stop them.\n\nQuickly ([SPACE]) find the exit!", 14);
+		_hurryText = new FlxTypeText(20, 20, 600, "Hurry!\n\nYour phone battery is almost dead, only 98% remaning!\n\nDon't let it empty because Big Mommy is Watching (Over) You and will punish you if that happens.\n\nAnswer her messages ([J]) before she calls you and drains even more battery.\n\nAvoid thugs who will bully you around.\n\nHackers disguised as NPCs will follow you and steal your wifi, bully them ([H]) to stop them.\n\nQuickly ([SPACE]) find the exit!", 14);
 		_hurryText.start(0.02, false, false, [SPACE], function()
 		{
 			_startDisplay.text = "Click or press SPACE to start";
