1046abcd7648d91831e973bee02bab931c9e6455
Epono <guillaume.ambrois@gmail.com>
2017-08-04 01:01:47 +0200

    POST LDJAM :
    Ajout des gifs + du monde au tuto + autre image promo

--- source/states/MenuState.hx
+++ source/states/MenuState.hx
@@ -71,8 +71,8 @@ class MenuState extends FlxState
 		_swagSprite = new FlxSprite().loadGraphic(AssetPaths.iDroidAnim_5__png, true, 80, 240);
 		_swagSprite.setPosition(530, 250);
 		_swagSprite.animation.add("lol", [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57], 19, false);
-		add(_swagSprite);
-		_swagSprite.animation.play("lol");
+		//add(_swagSprite);
+		//_swagSprite.animation.play("lol");
 		
 		_soundFadeIn.play();
 		FlxG.camera.fade(FlxColor.BLACK, .2, true);
